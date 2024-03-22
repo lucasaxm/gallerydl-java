4f98a36b1cce63f9aed11b641f8b48792818cd25
Lucas Morais <lucas.axm@gmail.com>
2024-03-22 19:21:31 -0300

    Initial commit

---
+++ src/main/java/com/lucasaxm/gallerydljava/GallerydlJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lucasaxm.gallerydljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallerydlJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GallerydlJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasaxm/gallerydljava/GallerydlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasaxm.gallerydljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallerydlJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
